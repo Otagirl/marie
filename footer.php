dea440f1131054193ad88075a508e640b1707389
marie-156-1998 <59875242+marie-156-1998@users.noreply.github.com>
2020-01-14 23:05:43 +0100

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,33 @@
+<footer>
+        <section id="footer_containt"> 
+            <div id="column">
+                <h1 id="title_column">Aide et informations</h1>
+                <p id="help_content">Modalité de paiement</p>
+                <p id="help_content">Livraison</p>
+                <p id="help_content">Suivi commande</p>    
+            </div>
+            <div id="column">
+                <h1 id="title_column"> Suivez-nous</h1>
+                <div id="one">
+                    <div id="line">
+                        <a  id="network" href="https://www.instagram.com/">
+                            <img id="img" src="sources/images/insta.png" alt="insta">
+                        </a>
+                        <p id="type">Instagram</p>
+                    </div>
+                    <div id="line">
+                        <a id="network" href="https://www.facebook.com/Jellyfish-Concept-105966642862580/">
+                            <img id="img" src="sources/images/facebook.png" alt="facebook">
+                        </a>
+                        <p id="type">facebook</p>   
+                    </div>
+                    <div id="line">
+                        <a href="https://twitter.com/">
+                            <img id="img" src="sources/images/twitter.png" alt="twitter">
+                        </a>
+                        <p id="type_twitter">Twitter</p>    
+                    </div>      
+                </div>
+            </div>
+        </section>
+    </footer>
