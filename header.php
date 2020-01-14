646a1d7d638c497bec094246d20a95ad10a6dba4
marie-156-1998 <59875242+marie-156-1998@users.noreply.github.com>
2020-01-14 23:01:23 +0100

    Update header.php

--- header.php
+++ header.php
@@ -1,41 +1,33 @@
 <!DOCTYPE html>
-<html <?php language_attributes(); ?>>
-    <head>
-        <meta charset="<?php bloginfo( 'charset' ); ?>" />
-
-        <!-- Appel du fichier style.css de notre thème -->
-        <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
-
-        <!--
-            Tout le contenu de la partie head de mon site
-         -->
-
-        <!-- Execution de la fonction wp_head() obligatoire ! -->
-        <?php wp_head(); ?>
-    </head>
-    <body <?php body_class(); ?>>
-        <header id="header">
-        <!-- Tout le contenu de l entête de mon site -->
-            <nav id="nav"> 
-                <img src="sources/images/politic-logo.png" alt="logo">
-                <a href="#">Nos idéaux</a>
-                <a href="#">Actualités</a>
-                <a href="#">Municipales 2020</a>
-                <a href="#"> S'engager</a>
-                <button>
-                <h1>Connexion</h1>
-                </button>
-                <button>
-                <h1>Adhérer</h1>
-                </button>
-                <button id="don">
-                <h1>Don</h1>
-                </button>
-            </nav>
-            <div></div>
-            <nav id="second">
-                <div id="recherche">
-                    <input type="text" placeholder="Recherche">
-                </div>
-            </nav>
-        </header>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<link rel="stylesheet" href="sources/style.css">    
+<header id="header">
+        <nav id="nav"> 
+            <img src="sources/images/politic-logo.png" alt="logo">
+            <a href="#">Nos idéaux</a>
+            <a href="#">Actualités</a>
+            <a href="#">Municipales 2020</a>
+            <a href="#"> S'engager</a>
+            <button>
+                <a href="#">
+                    <h1>Adhérer</h1>
+                </a>
+            </button>
+            <button id="don">
+                <a href="https://www.paypal.com/fr/signin">
+                    <h1>Don</h1>
+                </a>
+            </button>
+        </nav>
+        <nav id="second">
+            <div id="recherche">
+                <input type="text" placeholder="Recherche">
+            </div>
+        </nav>
+    </header>
